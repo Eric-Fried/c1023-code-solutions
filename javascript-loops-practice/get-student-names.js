544eea69a6041fcdcce8ec740f34228f04c5497b
Eric Fried <ebfried@me.com>
2023-10-26 16:59:18 +0000

    completed loops practice

---
+++ javascript-loops-practice/get-student-names.js
@@ -0,0 +1,9 @@
+/* exported getStudentNames */
+function getStudentNames(students) {
+  const newArray = [];
+  for (let i = 0; i < students.length; i++) {
+    newArray.push(students[i].name);
+  }
+
+  return newArray;
+}

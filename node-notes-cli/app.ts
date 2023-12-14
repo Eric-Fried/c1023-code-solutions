e1d13497636783f45892b507aef9035294f9b368
Eric Fried <ebfried@me.com>
2023-12-14 20:40:12 +0000

    added error handling

--- node-notes-cli/app.ts
+++ node-notes-cli/app.ts
@@ -7,33 +7,41 @@ type Data = {
 
 const [, , command, arg1, arg2] = process.argv;
 const contents = await readFile('data.json', { encoding: 'utf8' });
-// console.log(contents);
-// try {
-
-// } catch (err) {
-//   console.error('Error reading file', err);
-//   process.exit(1);
-// }
 
 const data: Data = JSON.parse(contents);
 
-switch (command) {
-  case 'read':
-    await readNotes();
-    break;
-  case 'create':
-    await addNote(arg1);
-    break;
-  case 'update':
-    await updateNote(arg1, arg2);
-    break;
-  case 'delete':
-    await deleteNote(arg1);
-    break;
-
-  default:
-    console.log('bad command ');
+async function write(data: Data): Promise<void> {
+  await writeFile('data.json', JSON.stringify(data, null, 2), 'utf8');
+}
+async function checkNote(note: string): Promise<void> {
+  if (!note) {
+    console.log('no note entered');
     process.exit(1);
+  }
+}
+
+try {
+  switch (command) {
+    case 'read':
+      await readNotes();
+      break;
+    case 'create':
+      await addNote(arg1);
+      break;
+    case 'update':
+      await updateNote(arg1, arg2);
+      break;
+    case 'delete':
+      await deleteNote(arg1);
+      break;
+
+    default:
+      console.log('bad command ');
+      process.exit(1);
+  }
+} catch (err) {
+  console.log(err);
+  process.exit(1);
 }
 
 async function readNotes(): Promise<void> {
@@ -43,21 +51,22 @@ async function readNotes(): Promise<void> {
 }
 
 async function addNote(note: string): Promise<void> {
+  checkNote(note);
   data.notes[data.nextId] = note;
   data.nextId++;
-  writeFile('data.json', JSON.stringify(data, null, 2), 'utf8');
+  await write(data);
   readNotes();
 }
 
 async function deleteNote(id: string): Promise<void> {
   delete data.notes[id];
-
-  writeFile('data.json', JSON.stringify(data, null, 2), 'utf8');
+  await write(data);
   readNotes();
 }
 
 async function updateNote(id: string, note: string): Promise<void> {
+  checkNote(note);
   data.notes[id] = note;
-  writeFile('data.json', JSON.stringify(data, null, 2), 'utf8');
+  await write(data);
   readNotes();
 }

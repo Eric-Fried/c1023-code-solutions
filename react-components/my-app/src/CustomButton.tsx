7fd55a3cdfa04998ee0ad1232aed4d59bb544cb9
Eric Fried <ebfried@me.com>
2023-12-21 01:13:43 +0000

    finished exercise and quiz

---
+++ react-components/my-app/src/CustomButton.tsx
@@ -0,0 +1,12 @@
+export default function CustomButton() {
+  return <button>Click ME</button>;
+}
+
+// export default function MyApp() {
+//   return (
+//     <div>
+//       <h1>Welcome to my app</h1>
+//       <CustomButton />
+//     </div>
+//   );
+// }

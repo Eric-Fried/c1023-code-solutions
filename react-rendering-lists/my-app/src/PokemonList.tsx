791e784d1570fba0043e67cfa6c6814767ce646f
Eric Fried <ebfried@me.com>
2024-01-11 01:22:47 +0000

    finished exercise and quiz

---
+++ react-rendering-lists/my-app/src/PokemonList.tsx
@@ -0,0 +1,12 @@
+export type Pokemon = {
+  number: string;
+  name: string;
+};
+
+type Props = {
+  pokedex: Pokemon[];
+};
+export function PokemonList({ pokedex }: Props) {
+  const listItems = pokedex.map((pokemon) => <li> {pokemon.name}</li>);
+  return <ul>{listItems}</ul>;
+}
